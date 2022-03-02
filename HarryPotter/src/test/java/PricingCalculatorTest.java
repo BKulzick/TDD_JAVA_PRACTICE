eeecb2aa02d66604ff50629cea1e84a3ab3649b9
Bryan Kulczycki <bryankulczycki@Bryans-MacBook-Pro.local>
2022-03-02 10:48:24 -0500

    updated with HP example

---
+++ HarryPotter/src/test/java/PricingCalculatorTest.java
@@ -0,0 +1,14 @@
+package java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PricingCalculatorTest {
+
+    @Test
+    void add_shouldReturnEightDollarsForOneHPBook() {
+        PricingCalculator calculator = new PricingCalculator();
+        assertEquals(false, true);
+    }
+}
